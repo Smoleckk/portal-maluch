d43b65d93ac9487931f7d52ba3ac1cf956b160ff
smoleckk <ksmolecki123@wp.pl>
2024-10-07 20:20:02 +0200

    dodanie bottom bar z mediami spolecznosciowymi

---
+++ web/maluch/projects/maluch-app/src/app/wspolne/nawigacja/nawigacja-media-spolecznosciowe/nawigacja-media-spolecznosciowe.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nawigacja-media-spolecznosciowe',
+  templateUrl: './nawigacja-media-spolecznosciowe.component.html',
+  styleUrls: ['./nawigacja-media-spolecznosciowe.component.scss']
+})
+export class NawigacjaMediaSpolecznoscioweComponent {
+
+}
--- web/maluch/projects/maluch-app/src/app/wspolne/nawigacja/nawigacja.module.ts
+++ web/maluch/projects/maluch-app/src/app/wspolne/nawigacja/nawigacja.module.ts
@@ -2,15 +2,18 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {NawigacjaGornaComponent} from './nawigacja-gorna/nawigacja-gorna.component';
 import {RouterLink} from "@angular/router";
+import { NawigacjaMediaSpolecznoscioweComponent } from './nawigacja-media-spolecznosciowe/nawigacja-media-spolecznosciowe.component';
 
 
 @NgModule({
   declarations: [
 
-    NawigacjaGornaComponent
+    NawigacjaGornaComponent,
+     NawigacjaMediaSpolecznoscioweComponent
   ],
   exports: [
-    NawigacjaGornaComponent
+    NawigacjaGornaComponent,
+    NawigacjaMediaSpolecznoscioweComponent
   ],
   imports: [
     CommonModule,
